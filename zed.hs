1e9612269c7d08616065615708ee48a10fd6c5cb
Julian Chan <j.chan0506@gmail.com>
2018-10-07 20:10:00 -0700

    a start
    lol sorta have an idea of wat im doing??

--- zed.hs
+++ zed.hs
@@ -1,2 +1,10 @@
---begin
-type state = [int]
+
+type Space = Int
+type Rows = [Space]
+type Cols = [Space]
+
+--functions
+
+isUnique :: [Space] -> Bool
+isUnique [] = True;
+isUnique (h:t) = if (h `elem` t) then False else isUnique t 
