fae21a9ec0c8c03d433ed198d098f260ec4ff16e
Julian Chan <j.chan0506@gmail.com>
2018-10-08 22:21:31 -0700

    trying to count indices

--- zed.hs
+++ zed.hs
@@ -1,10 +1,22 @@
+import Data.List
 
 type Space = Int
 type Rows = [Space]
-type Cols = [Space]
+type Zed = [Rows]
+type Clue = [[Int]]
 
---functions
+--functions	
 
 isUnique :: [Space] -> Bool
 isUnique [] = True;
 isUnique (h:t) = if (h `elem` t) then False else isUnique t 
+
+--([1,3,2,2],[3,2,1,2],[2,2,1,3],[2,2,3,1])
+
+gameSize :: Clue -> Int
+gameSize clue = length clue
+
+--searchN :: [[Int]] -> Zed -> [Int] 
+--searchN clue zed = elemIndices 1 (head clue)
+
+ind c = elemIndices 1 c
